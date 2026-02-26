eab781061bbecbc5c14b5f53a48d745f20658202
David García Ramos <davgarram@alum.us.es>
2026-02-26 15:09:45 +0100

    Fix

--- app.js
+++ app.js
@@ -199,10 +199,12 @@ document.addEventListener('alpine:init', () => {
                 resultados[(fila[2]=="Sí"?4:0) + (fila[3]=="Sí"?2:0) + (fila[4]=="Sí"?1:0)]+=1;
             });
             let data = {
-                name: 'Conoce el juego',
+                name: `Conoce el juego (${resultados[7]+resultados[6]+resultados[5]+resultados[4]})`,
+                value: resultados[7]+resultados[6]+resultados[5]+resultados[4],
                 children: [
                 {
-                    name: 'Cambia',
+                    name: `Cambia (${resultados[7]+resultados[6]})`,
+                    value: resultados[7]+resultados[6],
                     children: [
                     {
                         name: 'Gana',
@@ -213,7 +215,8 @@ document.addEventListener('alpine:init', () => {
                         value: resultados[6],
                     }
                     ],
-                    name: 'No cambia',
+                    name: `No cambia (${resultados[5]+resultados[4]})`,
+                    value: resultados[5]+resultados[4],
                     children: [
                     {
                         name: 'Gana',
@@ -225,10 +228,12 @@ document.addEventListener('alpine:init', () => {
                     }
                     ]
                 }],
-                name: 'Conoce el juego',
+                name: `No conoce el juego (${resultados[3]+resultados[2]+resultados[1]+resultados[0]})`,
+                value: resultados[3]+resultados[2]+resultados[1]+resultados[0],
                 children: [
                 {
-                    name: 'Cambia',
+                    name: `Cambia (${resultados[3]+resultados[2]})`,
+                    value: resultados[3]+resultados[2],
                     children: [
                     {
                         name: 'Gana',
@@ -239,7 +244,8 @@ document.addEventListener('alpine:init', () => {
                         value: resultados[2],
                     }
                     ],
-                    name: 'No cambia',
+                    name: `No cambia (${resultados[1]+resultados[0]})`,
+                    value: resultados[1]+resultados[0],
                     children: [
                     {
                         name: 'Gana',
