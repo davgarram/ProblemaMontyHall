a6b23c4f9b745343ac20752c648d7f239215883d
davgarram <73554576+davgarram@users.noreply.github.com>
2026-03-02 16:27:44 +0100

    Update app.js hotfix

--- app.js
+++ app.js
@@ -218,7 +218,9 @@ document.addEventListener('alpine:init', () => {
         actualizarUi(){
             let resultados = Array(8).fill(0);
             this.datos.forEach(function (fila) {
-                resultados[(fila[1]=="Sí"?4:0) + (fila[3]=="Sí"?2:0) + (fila[4]=="Sí"?1:0)]+=1;
+                if(fila[0]){
+                    resultados[(fila[1]=="Sí"?4:0) + (fila[3]=="Sí"?2:0) + (fila[4]=="Sí"?1:0)]+=1;
+                }
             });
             let data = [{
                 name: `Conoce el juego (${resultados[7]+resultados[6]+resultados[5]+resultados[4]})`,
